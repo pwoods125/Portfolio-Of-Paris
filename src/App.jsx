d5c7b319db470bdbc672bfc4e6dec5a2caa48734
Paris Woods <148830068+pwoods125@users.noreply.github.com>
2024-05-19 23:23:44 -0400

    Basic-React-Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+      <div>
+        <Header />
+        <Navbar />
+        <Footer />
+      </div>
+  );
+}
+
+export default App;
