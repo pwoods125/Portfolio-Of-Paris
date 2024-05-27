8fd658444d75ba17e413d1104b344d347ad59a4e
Paris Woods <148830068+pwoods125@users.noreply.github.com>
2024-05-27 19:19:44 -0400

    About and Portfolio Completed

---
+++ src/components/Project.jsx
@@ -0,0 +1,21 @@
+export default function Project({ projects }) {
+    return (
+        <ul className="list-group">
+        {projects.map((project) => (
+          <li className="list-group-item" key={project.id}>
+            {`${project.name}`}
+            <br></br>
+            <a href={project.application}>Link to application</a>
+            <br></br>
+            <a href={project.githubLink}>Link to GitHub</a> 
+            <br></br>
+            <img src={project.image} width="700" height="400"></img>
+            <br></br>
+            <br></br>
+          </li>
+        ))}
+      </ul>
+    )
+}
+
+Project.propTypes
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>My name is Paris Woods, and I am a Temple University graduate with a Bachelors of Science in Public Health. After nearly 13 years in healthcare, I am currently expanding my knowledge within technology to pursue a career in full stack web development. I am very experienced with operating computers, computer applications, researching using various databases such as PubMed, and operating Microsoft Office (word, excel, publisher, and PowerPoint). Also, I have also developed skills with HTML, CSS, client and server side JavaScript, command-line applications, and deployment applications such as GitHub, and Heroku.</p>
+        <p>One other thing I pride myself on is my empathy skills and ability to connect with people emotionally. It is important to me to understand that everyone has their own story that may affect the way act and respond to certain actions. Regardless, everyone deserves respect because one never knows what someone else is going through or has overcome. I prefer to treat people the way I would want to be treated. I am confident in my ability to make a positive contribution with any organization that is willing to offer me that chance.</p>
+      </div>
+    );
+  }
